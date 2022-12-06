173ad0836c804aa2a38f50682a4a5511b40e53cf
alexanderVLG <dominorog323@gmail.com>
2022-12-06 12:42:07 +0300

    НАстройка реакт-роутера

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import '../../styles/Footer.css';
+import '../styles/Footer.css';
 import MobileFooter from "./MobileFooter";
 
 class Footer extends Component {
